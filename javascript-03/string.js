6b93010c7551ad521cb7062cc7abbb5cc0a44f5e
Nirajan Acharya <nirajan.acharya666@gmail.com>
2023-04-27 14:46:01 +0545

    string operation

---
+++ javascript-03/string.js
@@ -0,0 +1,13 @@
+/*const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);*/
+
+
+//Concatenating strings
+//const greeting = `Hello`;
+
+const name ="Chris"
+const shila= 'hi'
+const greeting = `Hello, ${shila}`;
+console.log(greeting);
+
+
