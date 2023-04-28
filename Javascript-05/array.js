ea4309c3cd9d7861cba483f8b9e9a24658a8f94e
Nirajan Acharya <nirajan.acharya666@gmail.com>
2023-04-28 21:29:16 +0545

    array related function

---
+++ Javascript-05/array.js
@@ -0,0 +1,12 @@
+// array operation 
+// to find array size 
+// const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+// console.log(shopping.length);  // 5
+
+// Accessing and modifying array items
+// console.log(shopping[3])
+//it gives cheese as a output because cheese is on array index 3
+shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+shopping[0] = 'tahini';
+console.log(shopping);
+// shopping will now return [ "tahini", "milk", "cheese", "hummus", "noodles" ]
