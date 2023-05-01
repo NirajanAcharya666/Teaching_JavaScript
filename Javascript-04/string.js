be996fe156552b3dfa658725eaf6f64a94df7c0d
Nirajan Acharya <nirajan.acharya666@gmail.com>
2023-05-01 17:52:44 +0545

    Javascript course

--- Javascript-04/string.js
+++ Javascript-04/string.js
@@ -1,14 +1,14 @@
 //string method or string function 
 //length of string 
 
-/* const browserType = 'mozilla';
-console.log(browserType.length);
-*/
+// const browserType = 'mozilla is a good boy';
+// console.log(browserType.length);
+
 
 /*
 finding the index of substring
 
-let str = 'I am college topper'
+let str = 'I am college getafe topper'
 var a= str.indexOf('ge') // finding first index of string 
 var b= str.lastIndexOf('ge') // finding last index of string 
 console.log(a)
@@ -18,11 +18,11 @@ console.log(b) */
 // it takes (start, end position)
 
 // let x = 'This is the best'
-// console.log(x.slice(1,5)) // this has sliced from 1,5
+// console.log(x.slice(1,5)) -- space counted // this has sliced from 1,5
 
 //substr - it starts with one index and take required nummber as it needed
-// var y = 'Tea or Cofee you want to drink'
-// console.log(y.substr(2,7))
+var y = 'Tea or Cofee you want to drink'
+console.log(y.substr(2,7))
 
 //if you want to replace the sub-string then 
 //suppose if we want to replace tea with milk 
@@ -33,7 +33,7 @@ console.log(b) */
 //concatenate the string in javascript
 
 const str1 = 'Hello';
-const str2 = str1.concat(' ', 'World');
+const str2 = str1.concat(' ', 'this is my room')
 
 // 'Hello'. Strings are immutable, so `concat()` does not modify `str1`
 console.log(str1);
